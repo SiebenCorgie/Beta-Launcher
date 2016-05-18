dbe0901331d6f924fe9c2f239385f68d03711ec2
Siebencorgie <siebencorgie@googlemail.com>
2016-05-18 17:46:02 +0200

    Change the install file to copy main programm to /usr/share/Beta-Launcher , cinfuc files to /etc/Beta-Launcher, UnrealEngine.desktop to /usr/share/applications and the Beta-Launcher file (for running the programm) to /usr/share.

--- Install.sh
+++ Install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-cd /opt
-sudo git clone https://github.com/SiebenCorgie/Beta-Launcher.git
-sudo cp /opt/Beta-Launcher/Beta-Launcher /bin
-sudo cp /opt/Beta-Launcher/UnrealEngine.desktop /usr/share/applications
-cd /opt/Beta-Launcher
+cd /usr/share/
+sudo mkdir Beta-Launcher
+cd Beta-Launcher	
+sudo git clone -b installable https://github.com/SiebenCorgie/Beta-Launcher.git
+sudo cp /usr/share/Beta-Launcher/Beta-Launcher /usr/share/
+chmod +x /usr/share/Beta-Launcher
+sudo cp /usr/share/Beta-Launcher/UnrealEngine.desktop /usr/share/applications
+cd /etc
+mkdir Beta-Launcher
+cd Beta-Launcher
+cp /usr/share/Beta-Launcher/settings.conf /etc/Beta-Launcher
+cp /usr/share/Beta-Launcher/defaults.conf /etc/Beta-Launcher
 sudo chmod 666 settings.conf
 sudo chmod +r defaults.conf
-cd /bin 
-sudo chmod +x Beta-Launcher
 cd /usr/share/applications
 sudo chmod +x UnrealEngine.desktop
