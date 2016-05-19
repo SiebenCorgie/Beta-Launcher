12fbfe08715beaee56fd486bd1ed44317c7bd04f
Siebencorgie <siebencorgie@googlemail.com>
2016-05-19 14:16:56 +0200

    added new features to the install script, to follow the arch AUR guidlines, still not final...

--- Install.sh
+++ Install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 cd /usr/share/
-sudo mkdir Beta-Launcher
-cd Beta-Launcher	
+echo moving_to_/usr/share	
 sudo git clone -b installable https://github.com/SiebenCorgie/Beta-Launcher.git
-sudo cp /usr/share/Beta-Launcher/Beta-Launcher /usr/share/
-chmod +x /usr/share/Beta-Launcher
-sudo cp /usr/share/Beta-Launcher/UnrealEngine.desktop /usr/share/applications
-cd /etc
-mkdir Beta-Launcher
 cd Beta-Launcher
-cp /usr/share/Beta-Launcher/settings.conf /etc/Beta-Launcher
-cp /usr/share/Beta-Launcher/defaults.conf /etc/Beta-Launcher
+echo moved_to_it
+sudo cp /usr/share/Beta-Launcher/Beta-Launcher /bin
+echo Copyied_Beta-Launcher_to_/bin
+sudo cp /usr/share/Beta-Launcher/UnrealEngine.desktop /usr/share/applications
+echo Copied_DektopFile_To_/usr/share/applications
+cd /etc
+sudo mkdir Beta-Launcher
+cd Beta-Launcher
+sudo cp /usr/share/Beta-Launcher/settings.conf /etc/Beta-Launcher
+sudo cp /usr/share/Beta-Launcher/defaults.conf /etc/Beta-Launcher
+echo Copied_All_Conf_Files
 sudo chmod 666 settings.conf
 sudo chmod +r defaults.conf
 cd /usr/share/applications
 sudo chmod +x UnrealEngine.desktop
+sudo chmod +x /bin/Beta-Launcher
+echo changed_all_permissions
+echo END
