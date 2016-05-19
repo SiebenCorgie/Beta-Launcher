4f6bbee80c6664f7c586008afb8da0ef94a18874
Siebencorgie <siebencorgie@googlemail.com>
2016-05-19 18:05:50 +0200

    added icon path to Remove.sh

--- Remove.sh
+++ Remove.sh
@@ -4,4 +4,5 @@ sudo rm -rf /usr/share/Beta-Launcher
 sudo rm /bin/Beta-Launcher
 sudo rm -rf /etc/Beta-Launcher
 sudo rm /usr/share/applications/Beta-Launcher.desktop
+sudo rm /usr/share/pixmaps/Beta-Launcher.png
 echo END
